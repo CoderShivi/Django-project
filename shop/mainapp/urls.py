e4607b412d6465afda2475056fb9d2429da284e8
Shivani <108396731+CoderShivi@users.noreply.github.com>
2025-02-18 17:42:23 +0530

    edit

--- shop/mainapp/urls.py
+++ shop/mainapp/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import homeView, aboutView, contactView # importing the views from the mainapp to access the function
-
+from .views import homeView, aboutView, contactView,AddProduct,ProductDetails,Updateproduct,DeleteProduct # importing the views from the mainapp to access the function
+#
 urlpatterns =[
     path("",homeView, name='home'),
     path("about",aboutView,name='about'),
-    path("contacts",contactView, name='contact')
+    path("contacts",contactView, name='contact'),
+
+    path('Products/<int:pk>',      ProductDetails.as_view(),  name='prod_details'),#C
+    path('products/add',           AddProduct.as_view(),      name='add_prod'),#R
+    path('products/edit/<int:pk>', Updateproduct.as_view(),   name='edit_prod'),#U
+    path('products/del/<int:pk>',  DeleteProduct.as_view(),   name='del_prod')#D
+    
 ]
--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Product
 
+# importing url resolution tools
+from django.urls import reverse, reverse_lazy
+
 # to help load the template file
 from django.template import loader
 
@@ -8,7 +11,8 @@ from django.template import loader
 from django.http import HttpResponse
 
 # importing the generic class based views for CRUD operations
-from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic import CreateView,DetailView,UpdateView, DeleteView
+
 
 # Create your views here.
 def homeView(request):
@@ -38,4 +42,54 @@ def contactView(request):
     }
     template = loader.get_template('contacts.html')
     return HttpResponse(template.render(context,request))
+
+# Views for adding, editing and deleting products CRUD
+
+
+#Create
+class AddProduct(CreateView):
+    model=Product
+    fields = ['name','price','desc','pic','stock']
+    template_name='addproduct.html'
+    success_url=reverse_lazy('home')
+
+# Read -> show details of each product
+class ProductDetails(DetailView):
+    model=Product
+    template_name ='prod_details.html'
+    context_object_name='prod'
+
+# update ->
+class Updateproduct(UpdateView):
+    model = Product
+    fields ='__all__'
+    template_name = 'editProduct.html'
+
+    #Overriding a method to produce 
+    def get_success_url(self):
+        return reverse('prod_details',kwargs={'pk':self.object.pk})
+    
+# Delete
+class DeleteProduct(DeleteView):
+    model=Product
+    template_name='delProduct.html'
+    success_url=reverse_lazy('home')
+
+# search result 
+def searchView(request):
+    query=request.GET.get('search_text')
+    #fetch the query text from GET request
+
+    result =Product.objects.filter(name_icontains=query)
+    # collect the product objects matching the name
+    # This runs 'SELECT * FROM product WHERE name like  '%<query>%';
+    # icontains is case-insensitive
+
+    context = {
+        'items':result,
+        'query':query
+    }
+    template = loader.get_template('searchResult.html')
+    return HttpResponse(template.render(context,request))
+
 
